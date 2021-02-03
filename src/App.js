198dbb1e0c34050c5fdb8102fe20fab5f725e60d
Cristian Fonseca <cristiancomas@hotmail.com>
2021-02-02 20:54:12 -0500

    Navbar Implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Header from "./components/header"
+
+
 
 function App() {
   return (
-
-  );
+    <>
+    <Header/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import "../styles/styles.css"
+import 'boxicons'
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+const Header = () => {
+    return (
+        <header class="l-header">
+            <nav class="nav bd-grid">
+                <div>
+                    <a href="cfonseca.netlify.app" class="nav__logo">Cristian Fonseca</a>
+                </div>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
+                        <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+                        <li class="nav__item"><a href="#skills" class="nav__link">Skills</a></li>
+                        <li class="nav__item"><a href="#work" class="nav__link">Work</a></li>
+                        <li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="nav__toggle" id="nav-toggle">
+                    <box-icon name='menu' color='#0e2431' ></box-icon>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
